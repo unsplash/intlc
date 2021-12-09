8f420820b4fcbd8931c5b909ff3eb7c1908c7a2d
Sam A. Horvath-Hunt <hello@samhh.com>
2021-12-09 17:50:29 +0000

    Add primitive compiler

---
+++ lib/Intlc/Compiler.hs
@@ -0,0 +1,12 @@
+module Intlc.Compiler where
+
+import           Intlc.Core
+import           Prelude
+
+translation :: Translation -> Text
+translation (Static x)   = "'" <> x <> "'"
+translation (Dynamic xs) = "`" <> foldMap token xs <> "`"
+
+token :: Token -> Text
+token (Plaintext x)     = x
+token (Interpolation x) = "${" <> x <> "}"
