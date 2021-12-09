4c53bf464e93f9f81f2a332bcb1eb2bef56aaf4e
Sam A. Horvath-Hunt <hello@samhh.com>
2021-12-09 18:37:37 +0000

    Compile interpolated strings to lambdas

--- lib/Intlc/Compiler.hs
+++ lib/Intlc/Compiler.hs
@@ -1,12 +1,23 @@
 module Intlc.Compiler where
 
+import           Control.Monad.Writer
+import qualified Data.Text            as T
 import           Intlc.Core
 import           Prelude
 
+type Compiler = Writer [Arg]
+
 translation :: Translation -> Text
 translation (Static x)   = "'" <> x <> "'"
-translation (Dynamic xs) = "`" <> foldMap token xs <> "`"
+translation (Dynamic xs) = lambda args <> "`" <> str <> "`"
+  where (str, args) = runWriter $ foldMapM token xs
 
-token :: Token -> Text
-token (Plaintext x)           = x
-token (Interpolation (Arg x)) = "${" <> x <> "}"
+lambda :: [Arg] -> Text
+lambda xs = "({ " <> val <> " }: { " <> typ <> " }) => "
+  where val = T.intercalate ", " xs'
+        typ = T.intercalate "; " (xs' <&> (<> ": string"))
+        xs' = coerce xs
+
+token :: Token -> Compiler Text
+token (Plaintext x)     = pure x
+token (Interpolation x) = "${" <> coerce x <> "}" <$ tell (pure x)
