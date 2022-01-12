af5a18e74b08714e1de764634a3cc36456cbcd76
Sam A. Horvath-Hunt <hello@samhh.com>
2022-01-12 19:04:57 +0000

    Reorganise compiler to support different backends

--- lib/Intlc/Compiler.hs
+++ lib/Intlc/Compiler.hs
@@ -1,39 +1,12 @@
 module Intlc.Compiler where
 
-import           Control.Monad.Writer
-import qualified Data.Map             as M
-import qualified Data.Text            as T
+import qualified Data.Map                               as M
+import qualified Intlc.Compiler.Backend.TypeScript      as TS
 import           Intlc.Core
 import           Prelude
 
-type Compiler = Writer [Arg]
-
 dataset :: Dataset Translation -> Text
-dataset = M.foldMapWithKey export
-  where export k (Translation v _) = "export const " <> k <> " = " <> msg v <> newline
+dataset = M.foldMapWithKey translation
 
-msg :: Message -> Text
-msg (Static x)   = "'" <> x <> "'"
-msg (Dynamic xs) = lambda args <> "`" <> str <> "`"
-  where (str, args) = runWriter $ foldMapM token xs
-
-typedInput :: Maybe ICUType -> Text
-typedInput Nothing             = "string"
-typedInput (Just Number)       = "number"
-typedInput (Just (Callback _)) = "(x: string) => string"
-
-lambda :: [Arg] -> Text
-lambda xs = "(x: { " <> typ <> " }) => "
-  where typ = T.intercalate "; " (prop <$> xs)
-        prop (Arg n mt) = n <> ": " <> typedInput mt
-
-token :: Token -> Compiler Text
-token (Plaintext x)               = pure x
-token (Interpolation x@(Arg n (Just (Callback xs)))) = do
-  tell . pure $ x
-  children <- foldMapM token xs
-  pure $ "${x." <> n <> "(`" <> children <> "`)}"
-token (Interpolation x@(Arg n _)) = "${x." <> n <> "}" <$ tell (pure x)
-
-newline :: Text
-newline = "\n"
+translation :: Text -> Translation -> Text
+translation k (Translation v TypeScript)      = TS.export k v
---
+++ lib/Intlc/Compiler/Backend/TypeScript.hs
@@ -0,0 +1,37 @@
+module Intlc.Compiler.Backend.TypeScript where
+
+import           Control.Monad.Writer
+import qualified Data.Text            as T
+import           Intlc.Core
+import           Prelude
+
+type Compiler = Writer [Arg]
+
+export :: Text -> Message -> Text
+export k v = "export const " <> k <> " = " <> msg v <> newline
+
+msg :: Message -> Text
+msg (Static x)   = "'" <> x <> "'"
+msg (Dynamic xs) = lambda args <> "`" <> str <> "`"
+  where (str, args) = runWriter $ foldMapM token xs
+
+typedInput :: Maybe ICUType -> Text
+typedInput Nothing             = "string"
+typedInput (Just Number)       = "number"
+typedInput (Just (Callback _)) = "(x: string) => string"
+
+lambda :: [Arg] -> Text
+lambda xs = "(x: { " <> typ <> " }) => "
+  where typ = T.intercalate "; " (prop <$> xs)
+        prop (Arg n mt) = n <> ": " <> typedInput mt
+
+token :: Token -> Compiler Text
+token (Plaintext x)               = pure x
+token (Interpolation x@(Arg n (Just (Callback xs)))) = do
+  tell . pure $ x
+  children <- foldMapM token xs
+  pure $ "${x." <> n <> "(`" <> children <> "`)}"
+token (Interpolation x@(Arg n _)) = "${x." <> n <> "}" <$ tell (pure x)
+
+newline :: Text
+newline = "\n"
