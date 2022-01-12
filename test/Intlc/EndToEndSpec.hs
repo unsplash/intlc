491d420955ea477d1a1bddc170719bfcc55f9562
Sam A. Horvath-Hunt <hello@samhh.com>
2022-01-12 19:34:11 +0000

    Improve legibility of end-to-end JSON inputs

--- test/Intlc/EndToEndSpec.hs
+++ test/Intlc/EndToEndSpec.hs
@@ -5,6 +5,7 @@ import           Intlc.Compiler       (dataset)
 import           Intlc.Parser         (parseDataset)
 import           Prelude              hiding (ByteString)
 import           Test.Hspec
+import           Text.RawString.QQ    (r)
 
 (=*=) :: ByteString -> Text -> IO ()
 x =*= y = f x `shouldBe` Right y
@@ -13,13 +14,13 @@ x =*= y = f x `shouldBe` Right y
 spec :: Spec
 spec = describe "end-to-end" $ do
   it "example message" $ do
-        "{ \"title\": { \"message\": \"Unsplash\" }, \"greeting\": { \"message\": \"Hello <bold>{name}</bold>, {age, number}!\", \"backend\": \"ts\" } }"
-    =*= "export const greeting = (x: { bold: (x: string) => string; name: string; age: number }) => `Hello ${x.bold(`${x.name}`)}, ${x.age}!`\nexport const title = 'Unsplash'\n"
+    [r|{ "title": { "message": "Unsplash" }, "greeting": { "message": "Hello <bold>{name}</bold>, {age, number}!", "backend": "ts" } }|]
+      =*= "export const greeting = (x: { bold: (x: string) => string; name: string; age: number }) => `Hello ${x.bold(`${x.name}`)}, ${x.age}!`\nexport const title = 'Unsplash'\n"
 
   it "TypeScript backend" $ do
-        "{ \"f\": { \"message\": \"{x} <z>{y, number}</z>\", \"backend\": \"ts\" } }"
-    =*= "export const f = (x: { x: string; z: (x: string) => string; y: number }) => `${x.x} ${x.z(`${x.y}`)}`\n"
+    [r|{ "f": { "message": "{x} <z>{y, number}</z>", "backend": "ts" } }|]
+      =*= "export const f = (x: { x: string; z: (x: string) => string; y: number }) => `${x.x} ${x.z(`${x.y}`)}`\n"
 
   it "TypeScriptReact backend" $ do
-        "{ \"f\": { \"message\": \"{x} <z>{y, number}</z>\", \"backend\": \"tsx\" } }"
-    =*= "export const f = (x: { x: ReactNode; z: (x: ReactNode) => ReactNode; y: number }) => <>{x.x} {x.z(<>{x.y}</>)}</>\n"
+    [r|{ "f": { "message": "{x} <z>{y, number}</z>", "backend": "tsx" } }|]
+      =*= "export const f = (x: { x: ReactNode; z: (x: ReactNode) => ReactNode; y: number }) => <>{x.x} {x.z(<>{x.y}</>)}</>\n"
