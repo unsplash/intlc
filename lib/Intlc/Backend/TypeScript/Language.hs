7e2e3e28328948a097ad2bb8cfed25e342b665eb
Sam A. Horvath-Hunt <hello@samhh.com>
2022-02-07 13:31:09 +0000

    Don't lift to list in TS AST creation
    Nowhere in these functions will we ever actually produce more than one `In`.

--- lib/Intlc/Backend/TypeScript/Language.hs
+++ lib/Intlc/Backend/TypeScript/Language.hs
@@ -14,6 +14,7 @@ data TypeOf = Lambda Args Out
   deriving (Show, Eq)
 
 -- Avoid `Map` due to its `Ord` constraint.
+type UncollatedArgs = [(Text, In)]
 type Args = [(Text, NonEmpty In)]
 
 data Uni
@@ -42,58 +43,58 @@ isMultiUnion (TNumLitUnion xs) = length xs > 1
 isMultiUnion _                 = False
 
 -- Collate arguments with the same name.
-collateArgs :: Args -> Args
+collateArgs :: UncollatedArgs -> Args
 collateArgs = reverse . fmap (second nub) . go [] where
-  go acc []            = acc
-  go acc (x@(n,ys):xs) =
+  go acc []         = acc
+  go acc ((n,t):xs) =
     case findIndex ((== n) . fst) acc of
-      Nothing -> go (x:acc) xs
-      Just i  -> go (acc & ix i %~ second (<> ys)) xs
+      Nothing -> go ((n, pure t):acc) xs
+      Just i  -> go (acc & ix i %~ second (<> pure t)) xs
 
 fromMsg :: Out -> ICU.Message -> TypeOf
 fromMsg x ICU.Static {}    = Lambda mempty x
 fromMsg x (ICU.Dynamic ys) = Lambda (collateArgs (fromToken =<< toList ys)) x
 
-fromToken :: ICU.Token -> Args
+fromToken :: ICU.Token -> UncollatedArgs
 fromToken ICU.Plaintext {}      = mempty
 fromToken (ICU.Interpolation x) = fromArg x
 
-fromArg :: ICU.Arg -> Args
-fromArg (ICU.Arg n ICU.String)         = pure (n, pure $ TUniIn TStr)
-fromArg (ICU.Arg n ICU.Number)         = pure (n, pure TNum)
-fromArg (ICU.Arg n ICU.Date {})        = pure (n, pure TDate)
-fromArg (ICU.Arg n ICU.Time {})        = pure (n, pure TDate)
+fromArg :: ICU.Arg -> UncollatedArgs
+fromArg (ICU.Arg n ICU.String)         = pure (n, TUniIn TStr)
+fromArg (ICU.Arg n ICU.Number)         = pure (n, TNum)
+fromArg (ICU.Arg n ICU.Date {})        = pure (n, TDate)
+fromArg (ICU.Arg n ICU.Time {})        = pure (n, TDate)
 fromArg (ICU.Arg n (ICU.Plural x))     = fromPlural n x
-fromArg (ICU.Arg n (ICU.Select cs mw)) = (n, pure t) : (fromSelectCase =<< toList cs) <> foldMap fromSelectWildcard mw
+fromArg (ICU.Arg n (ICU.Select cs mw)) = (n, t) : (fromSelectCase =<< toList cs) <> foldMap fromSelectWildcard mw
   -- When there's no wildcard case we can compile to a union of string literals.
   where t = case mw of
               Just _  -> TUniIn TStr
               Nothing -> TStrLitUnion $ caseLit <$> cs
         caseLit (ICU.SelectCase x _) = x
-fromArg (ICU.Arg n (ICU.Callback xs))  = (n, pure TEndo) : (fromToken =<< xs)
+fromArg (ICU.Arg n (ICU.Callback xs))  = (n, TEndo) : (fromToken =<< xs)
 
-fromPlural :: Text -> ICU.Plural -> Args
-fromPlural n (ICU.Cardinal (ICU.LitPlural ls mw))      = (n, pure t) : (fromExactPluralCase =<< toList ls) <> foldMap fromPluralWildcard mw
+fromPlural :: Text -> ICU.Plural -> UncollatedArgs
+fromPlural n (ICU.Cardinal (ICU.LitPlural ls mw))      = (n, t) : (fromExactPluralCase =<< toList ls) <> foldMap fromPluralWildcard mw
   -- When there's no wildcard case we can compile to a union of number literals.
   where t = case mw of
               Just _  -> TNum
               Nothing -> TNumLitUnion $ caseLit <$> ls
         caseLit (ICU.PluralCase (ICU.PluralExact x) _) = x
-fromPlural n (ICU.Cardinal (ICU.RulePlural rs w))      = (n, pure TNum) : (fromRulePluralCase =<< toList rs) <> fromPluralWildcard w
-fromPlural n (ICU.Cardinal (ICU.MixedPlural ls rs w))  = (n, pure TNum) : (fromExactPluralCase =<< toList ls) <> (fromRulePluralCase =<< toList rs) <> fromPluralWildcard w
-fromPlural n (ICU.Ordinal (ICU.OrdinalPlural ls rs w)) = (n, pure TNum) : (fromExactPluralCase =<< ls) <> (fromRulePluralCase =<< toList rs) <> fromPluralWildcard w
+fromPlural n (ICU.Cardinal (ICU.RulePlural rs w))      = (n, TNum) : (fromRulePluralCase =<< toList rs) <> fromPluralWildcard w
+fromPlural n (ICU.Cardinal (ICU.MixedPlural ls rs w))  = (n, TNum) : (fromExactPluralCase =<< toList ls) <> (fromRulePluralCase =<< toList rs) <> fromPluralWildcard w
+fromPlural n (ICU.Ordinal (ICU.OrdinalPlural ls rs w)) = (n, TNum) : (fromExactPluralCase =<< ls) <> (fromRulePluralCase =<< toList rs) <> fromPluralWildcard w
 
-fromExactPluralCase :: ICU.PluralCase ICU.PluralExact -> Args
+fromExactPluralCase :: ICU.PluralCase ICU.PluralExact -> UncollatedArgs
 fromExactPluralCase (ICU.PluralCase (ICU.PluralExact _) xs) = fromToken =<< xs
 
-fromRulePluralCase :: ICU.PluralCase ICU.PluralRule -> Args
+fromRulePluralCase :: ICU.PluralCase ICU.PluralRule -> UncollatedArgs
 fromRulePluralCase (ICU.PluralCase _ xs) = fromToken =<< xs
 
-fromPluralWildcard :: ICU.PluralWildcard -> Args
+fromPluralWildcard :: ICU.PluralWildcard -> UncollatedArgs
 fromPluralWildcard (ICU.PluralWildcard xs) = fromToken =<< xs
 
-fromSelectCase :: ICU.SelectCase -> Args
+fromSelectCase :: ICU.SelectCase -> UncollatedArgs
 fromSelectCase (ICU.SelectCase _ xs) = fromToken =<< xs
 
-fromSelectWildcard :: ICU.SelectWildcard -> Args
+fromSelectWildcard :: ICU.SelectWildcard -> UncollatedArgs
 fromSelectWildcard (ICU.SelectWildcard xs) = fromToken =<< xs
