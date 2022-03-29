57363dfc37855dfb57fc0c7544f7799c293d845a
Sam A. Horvath-Hunt <hello@samhh.com>
2022-03-29 14:26:37 +0100

    Add tests for TS argument collection within interpolations
    The boolean test currently fails.

--- test/Intlc/Backend/TypeScriptSpec.hs
+++ test/Intlc/Backend/TypeScriptSpec.hs
@@ -4,6 +4,7 @@ import qualified Data.Text                         as T
 import           Intlc.Backend.JavaScript.Compiler (InterpStrat (..))
 import           Intlc.Backend.TypeScript.Compiler (compileNamedExport,
                                                     compileTypeof)
+import qualified Intlc.Backend.TypeScript.Language as TS
 import           Intlc.Core                        (Locale (Locale))
 import qualified Intlc.ICU                         as ICU
 import           Prelude                           hiding (ByteString)
@@ -87,3 +88,49 @@ spec = describe "TypeScript compiler" $ do
         let golden' = golden JSX (compileNamedExport JSX (Locale "te-ST") "test")
 
         golden' "named-export" msg
+
+  describe "collects nested arguments" $ do
+    let args (TS.Lambda xs _) = xs
+    let fromToken = args . TS.fromMsg TS.TFragment . ICU.Dynamic . pure . ICU.Interpolation . ICU.Arg "x"
+    let fromArgs = fromList
+
+    it "in select" $ do
+      let x = flip ICU.Select Nothing . pure $ ICU.SelectCase "foo" [ICU.Interpolation $ ICU.Arg "y" ICU.String]
+      let ys =
+              [ ("x", pure (TS.TStrLitUnion (pure "foo")))
+              , ("y", pure (TS.TUniIn TS.TStr))
+              ]
+      fromToken x `shouldBe` fromArgs ys
+
+    it "in cardinal plural" $ do
+      let x = ICU.Plural . ICU.Cardinal . flip ICU.LitPlural Nothing . pure $
+                ICU.PluralCase (ICU.PluralExact "42") [ICU.Interpolation $ ICU.Arg "y" ICU.String]
+      let ys =
+              [ ("x", pure (TS.TNumLitUnion (pure "42")))
+              , ("y", pure (TS.TUniIn TS.TStr))
+              ]
+      fromToken x `shouldBe` fromArgs ys
+
+    it "in ordinal plural" $ do
+      let x = ICU.Plural . ICU.Ordinal $ ICU.OrdinalPlural
+                [ICU.PluralCase (ICU.PluralExact "42") [ICU.Interpolation $ ICU.Arg "foo" (ICU.Date ICU.Short)]]
+                (pure $ ICU.PluralCase ICU.Few [ICU.Interpolation $ ICU.Arg "bar" ICU.String])
+                (ICU.PluralWildcard [ICU.Interpolation $ ICU.Arg "baz" ICU.Number])
+      let ys =
+              [ ("x", pure TS.TNum)
+              , ("foo", pure TS.TDate)
+              , ("bar", pure (TS.TUniIn TS.TStr))
+              , ("baz", pure TS.TNum)
+              ]
+      fromToken x `shouldBe` fromArgs ys
+
+    it "in boolean" $ do
+      let x = ICU.Bool
+                [ICU.Interpolation $ ICU.Arg "y" ICU.String]
+                [ICU.Interpolation $ ICU.Arg "z" ICU.Number]
+      let ys =
+              [ ("x", pure TS.TBool)
+              , ("y", pure (TS.TUniIn TS.TStr))
+              , ("z", pure TS.TNum)
+              ]
+      fromToken x `shouldBe` fromArgs ys
