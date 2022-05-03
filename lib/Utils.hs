26c256f23a8c29310b943d0e36e417e95db37a5b
Sam A. Horvath-Hunt <hello@samhh.com>
2022-05-03 12:58:23 +0100

    Add toNubMap util

--- lib/Utils.hs
+++ lib/Utils.hs
@@ -1,6 +1,7 @@
 module Utils where
 
 import           Data.List (nub, (\\))
+import qualified Data.Map  as M
 import           Prelude
 
 -- Borrowed from: https://hackage.haskell.org/package/intro-0.9.0.0/docs/Intro.html#v:-60--62--94-
@@ -14,3 +15,9 @@ infixr 6 <>^
 -- once in the output regardless of how often they reoccur.
 dupes :: Ord a => [a] -> [a]
 dupes xs = nub . sort $ xs \\ nub xs
+
+-- Like `M.fromList`, but instead calls the given function in the presence of
+-- duplicate keys. The applicative context allows for easy interop with things
+-- like parsers.
+toNubMap :: Applicative m => (NonEmpty Text -> m (Map Text a)) -> [(Text, a)] -> m (Map Text a)
+toNubMap f xs = maybe (pure $ M.fromList xs) f . nonEmpty . dupes . fmap fst $ xs
--- test/Intlc/UtilsSpec.hs
+++ test/Intlc/UtilsSpec.hs
@@ -1,8 +1,9 @@
 module Intlc.UtilsSpec (spec) where
 
+import qualified Data.Map   as M
 import           Prelude
 import           Test.Hspec
-import           Utils      (dupes)
+import           Utils      (dupes, toNubMap)
 
 spec :: Spec
 spec = describe "utils" $ do
@@ -14,3 +15,16 @@ spec = describe "utils" $ do
 
     it "returns duplicates only once each" $ do
       f "abbcddde" `shouldBe` "bd"
+
+  describe "toNubMap" $ do
+    let f = toNubMap
+
+    it "equivalent to M.fromList in the absence of duplicate keys" $ do
+      let xs = [("a", 1), ("b", 2)] :: [(Text, Int)]
+      f (error "absurd") xs `shouldBe` [M.fromList xs]
+
+    it "calls input function in the presence of duplicate keys" $ do
+      let onFail = pure . M.fromList . fmap (,42) . toList
+      let xs = zip ["a", "b", "a", "c", "c", "d"] [1..] :: [(Text, Int)]
+      let ys = zip ["a", "c"] (repeat 42)
+      f onFail xs `shouldBe` [M.fromList ys]
