af9013c56ba362e1b13b88292cc884d56eedd791
Thomas Lefebvre <thmslbvr@gmail.com>
2022-05-27 12:56:34 -0400

    Invoke linter (WIP)

--- internal/Main.hs
+++ internal/Main.hs
@@ -1,9 +1,29 @@
 module Main where
 
-import           CLI       (Opts (..), getOpts)
-import qualified Data.Text as T
+import           CLI                (Opts (..), getOpts)
+import           Intlc.Core
+import           Intlc.Linter
+import           Intlc.Parser       (parseDataset, printErr)
+import           Intlc.Parser.Error (ParseFailure)
 import           Prelude
+
 
 main :: IO ()
 main = getOpts >>= \case
-  Lint x -> putTextLn $ "Hello, " <> T.pack x <> "!"
+  Lint path -> either parserDie (mkStatus . go) =<< getParsed path
+  where
+    getParsed :: FilePath -> IO (Either ParseFailure (Dataset Translation))
+    getParsed x = parseDataset x <$> readFileText x
+
+    parserDie = die . printErr
+
+    -- TODO we could short-circuit to the first error or accumulate errors
+    go :: Dataset Translation -> Status
+    go = foldr (\a b -> lint (message a)) Success
+
+    mkStatus :: Status -> IO ()
+    mkStatus = \case
+      Success   -> putTextLn (show Success)
+      Failure x -> die (show x)
+
+
