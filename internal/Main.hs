d0310bdf66c220cba479d66f3eeffc0358a75f76
Thomas Lefebvre <thmslbvr@gmail.com>
2022-05-27 16:18:06 -0400

    More readable format

--- internal/Main.hs
+++ internal/Main.hs
@@ -1,29 +1,30 @@
 module Main where
 
 import           CLI                (Opts (..), getOpts)
+import           Data.Map           (filter, size, toAscList)
 import           Intlc.Core
 import           Intlc.Linter
 import           Intlc.Parser       (parseDataset, printErr)
 import           Intlc.Parser.Error (ParseFailure)
-import           Prelude
+import           Prelude            hiding (filter)
 
 
 main :: IO ()
 main = getOpts >>= \case
-  Lint path -> either parserDie (mkStatus . go) =<< getParsed path
+  Lint path -> either parserDie lint' =<< getParsed path
   where
     getParsed :: FilePath -> IO (Either ParseFailure (Dataset Translation))
     getParsed x = parseDataset x <$> readFileText x
 
     parserDie = die . printErr
 
-    -- TODO we could short-circuit to the first error or accumulate errors
-    go :: Dataset Translation -> Status
-    go = foldr (\a b -> lint (message a)) Success
+    lint' :: Dataset Translation -> IO ()
+    lint' = exit . filter isFailure . fmap (lint . message)
 
-    mkStatus :: Status -> IO ()
-    mkStatus = \case
-      Success   -> putTextLn (show Success)
-      Failure x -> die (show x)
+    exit :: Dataset Status -> IO ()
+    exit sts
+      | size sts > 0 = die ((show . toAscList) sts)
+      | otherwise = putLTextLn "Success"
+
 
 
