1a21e1c6e570ee9f8d362ade3eaf07bf1c2b9513
Sam A. Horvath-Hunt <hello@samhh.com>
2022-05-31 13:48:30 +0100

    Hoist incrementing

--- lib/Intlc/Linter.hs
+++ lib/Intlc/Linter.hs
@@ -24,23 +24,24 @@ interpolationsRule = go 0
     go :: Int -> Stream -> Status
     go 2 _      = Failure TooManyInterpolations
     go _ []     = Success
-    go n (x:xs) = go n' $ ys <> xs
-      where (n', ys) = getStream x n
+    go n (x:xs) = go n' $ maybeToMonoid mys <> xs
+      where mys = getStream x
+            n' = n + length mys
 
-    getInterps :: Token -> Int -> (Int, Stream)
-    getInterps Plaintext {} n = (n, [])
-    getInterps (Interpolation _ String) n                                           = (n, [])
-    getInterps (Interpolation _ Number) n                                           = (n, [])
-    getInterps (Interpolation _ Date {}) n                                          = (n, [])
-    getInterps (Interpolation _ Time {}) n                                          = (n, [])
-    getInterps (Interpolation _ PluralRef) n                                        = (n, [])
-    getInterps (Interpolation _ Bool {trueCase, falseCase}) n                       = (n + 1, trueCase <> falseCase)
+    getStream :: Token -> Maybe Stream
+    getStream Plaintext {} = Nothing
+    getStream (Interpolation _ String)                                           = Nothing
+    getStream (Interpolation _ Number)                                           = Nothing
+    getStream (Interpolation _ Date {})                                          = Nothing
+    getStream (Interpolation _ Time {})                                          = Nothing
+    getStream (Interpolation _ PluralRef)                                        = Nothing
+    getStream (Interpolation _ Bool {trueCase, falseCase})                       = Just $ trueCase <> falseCase
 
     -- TODO: plural cases are really complicated to pattern match, is there a better way to handle all of this?
-    getInterps (Interpolation _ Plural {}) n                                        = (n, [])
-    getInterps (Interpolation _ (Select case' Nothing)) n                           = (n + 1, concatMap (\(SelectCase _ xs) -> xs) case')
-    getInterps (Interpolation _ (Select case' (Just (SelectWildcard wildcards)))) n = (n + 1, wildcards <> concatMap (\(SelectCase _ xs) -> xs) case')
-    getInterps (Interpolation _ (Callback xs)) n                                    = (n + 1, xs)
+    getStream (Interpolation _ Plural {})                                        = Just []
+    getStream (Interpolation _ (Select case' Nothing))                           = Just $ concatMap (\(SelectCase _ xs) -> xs) case'
+    getStream (Interpolation _ (Select case' (Just (SelectWildcard wildcards)))) = Just $ wildcards <> concatMap (\(SelectCase _ xs) -> xs) case'
+    getStream (Interpolation _ (Callback xs))                                    = Just xs
 
 lint :: Message -> Status
 lint (Message stream) = interpolationsRule stream
