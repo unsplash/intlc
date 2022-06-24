187c613efb804eaa204af4ac75e31fa4547facba
Sam A. Horvath-Hunt <hello@samhh.com>
2022-06-24 13:39:58 +0100

    Plug external linter into CLI
    Lots of code duplicated from the internal CLI for now.

--- cli/Main.hs
+++ cli/Main.hs
@@ -1,9 +1,11 @@
 module Main where
 
 import           CLI                (Opts (..), getOpts)
+import qualified Data.Map           as M
 import qualified Data.Text          as T
 import           Intlc.Compiler     (compileDataset, compileFlattened)
 import           Intlc.Core
+import           Intlc.Linter
 import           Intlc.Parser       (parseDataset, printErr)
 import           Intlc.Parser.Error (ParseFailure)
 import           Prelude
@@ -12,7 +14,13 @@ main :: IO ()
 main = getOpts >>= \case
   Compile path loc -> tryCompile loc =<< getParsed path
   Flatten path     -> either parserDie (putTextLn . compileFlattened) =<< getParsed path
-  Lint    path     -> pure ()
+  Lint    path     -> either parserDie lint =<< getParsed path
+    where lint = exit . M.mapMaybe (statusToMaybe . lintExternal . message)
+          exit sts
+            | M.size sts > 0 = die . T.unpack . ("Errors\n" <>) . M.foldrWithKey mkLine mempty $ sts
+            | otherwise = pure ()
+          mkLine k es acc = acc <> "\n" <> k <> ": " <> e
+            where e = T.intercalate ", " . toList . fmap show $ es
   where tryCompile l = either parserDie (either compilerDie putTextLn . compileDataset l)
         parserDie = die . printErr
         compilerDie = die . T.unpack . ("Invalid keys:\n" <>) . T.intercalate "\n" . fmap ("\t" <>) . toList
