fd9bc78aae2527bbc094824e397f3041e4c7c2e1
Sam A. Horvath-Hunt <hello@samhh.com>
2022-06-24 13:23:09 +0100

    Add Rule type alias

--- lib/Intlc/Linter.hs
+++ lib/Intlc/Linter.hs
@@ -12,6 +12,8 @@ data Status a
   | Failure (NonEmpty a)
   deriving (Eq, Show)
 
+type Rule a = Stream -> Maybe a
+
 statusToMaybe :: Status a -> Maybe (NonEmpty a)
 statusToMaybe Success      = Nothing
 statusToMaybe (Failure xs) = Just xs
@@ -20,17 +22,17 @@ maybeToStatus :: Maybe (NonEmpty a) -> Status a
 maybeToStatus Nothing   = Success
 maybeToStatus (Just xs) = Failure xs
 
-interpolationsRule :: Stream -> Maybe InternalLintingError
+interpolationsRule :: Rule InternalLintingError
 interpolationsRule = go 0
   where
-    go :: Int -> Stream -> Maybe InternalLintingError
+    go :: Int -> Rule InternalLintingError
     go 2 _      = Just TooManyInterpolations
     go _ []     = Nothing
     go n (x:xs) = go n' $ maybeToMonoid mys <> xs
       where mys = getStream x
             n' = n + length mys
 
-lintWith :: [Stream -> Maybe a] -> Message -> Status a
+lintWith :: [Rule a] -> Message -> Status a
 lintWith rules (Message stream) = toStatus $ rules `flap` stream
   where toStatus = maybeToStatus . nonEmpty . catMaybes
 
