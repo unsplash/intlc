6d2ce9122dbe930a602314051878d4716701aad1
Sam A. Horvath-Hunt <hello@samhh.com>
2022-06-27 15:17:46 +0100

    Test lint rules in isolation

--- test/Intlc/LinterSpec.hs
+++ test/Intlc/LinterSpec.hs
@@ -6,39 +6,44 @@ import           Intlc.Linter
 import           Prelude
 import           Test.Hspec
 
+lintWith' :: Rule a -> Message -> Status a
+lintWith' = lintWith . pure
+
 spec :: Spec
 spec = describe "linter" $ do
   describe "internal" $ do
     describe "interpolations" $ do
+      let lint = lintWith' interpolationsRule
+
       it "lints streams with 1 plain text token" $ do
-        lintInternal (Message [Plaintext "yay"]) `shouldBe` Success
+        lint (Message [Plaintext "yay"]) `shouldBe` Success
 
       it "lints streams with 2 or more plain text token" $ do
-        lintInternal (Message [Plaintext "yay", Plaintext "Hello"]) `shouldBe` Success
+        lint (Message [Plaintext "yay", Plaintext "Hello"]) `shouldBe` Success
 
       it "lints streams with 1 simple interpolation" $ do
-        lintInternal (Message [Interpolation "Hello" String]) `shouldBe` Success
+        lint (Message [Interpolation "Hello" String]) `shouldBe` Success
 
       it "lints streams with 1 complex interpolation" $ do
-        lintInternal (Message [Interpolation "Hello" (Callback [])]) `shouldBe` Success
+        lint (Message [Interpolation "Hello" (Callback [])]) `shouldBe` Success
 
       it "lints streams with 1 complex interpolation and 1 simple interpolation" $ do
-        lintInternal (Message [Interpolation "Hello" (Callback []), Plaintext "hello"]) `shouldBe` Success
+        lint (Message [Interpolation "Hello" (Callback []), Plaintext "hello"]) `shouldBe` Success
 
       it "does not lint streams with 2 or more complex interpolations" $ do
-        lintInternal (Message [Interpolation "Hello" (Callback []), Interpolation "Hello" (Bool [] [])]) `shouldBe` Failure (pure TooManyInterpolations)
+        lint (Message [Interpolation "Hello" (Callback []), Interpolation "Hello" (Bool [] [])]) `shouldBe` Failure (pure TooManyInterpolations)
 
       it "does not lint nested streams" $ do
-        lintInternal (Message [Interpolation "outer" (Callback [Interpolation "inner" (Callback [])])]) `shouldBe` Failure (pure TooManyInterpolations)
+        lint (Message [Interpolation "outer" (Callback [Interpolation "inner" (Callback [])])]) `shouldBe` Failure (pure TooManyInterpolations)
 
       it "does not lint complex interpolations with nested complex interpolations" $ do
-        lintInternal (Message [Interpolation "outer" (Select (This (pure $ SelectCase "hello" [Interpolation "super_inner" (Callback [])])))]) `shouldBe` Failure (pure TooManyInterpolations)
+        lint (Message [Interpolation "outer" (Select (This (pure $ SelectCase "hello" [Interpolation "super_inner" (Callback [])])))]) `shouldBe` Failure (pure TooManyInterpolations)
 
       it "stops iterating after encountering two stream-interpolations" $ do
         let nested x = Interpolation "x" (Callback [x])
         let e = error "should not reach this item"
 
-        lintInternal (Message
+        lint (Message
           [ nested (nested e)
           , e
           ]) `shouldBe` Failure (pure TooManyInterpolations)
