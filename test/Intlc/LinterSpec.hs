e0c1e36107d80430e4e18419fee11bf543fb5f02
nim442 <nimrat@unsplash.com>
2022-07-14 01:15:31 -0400

    fix and add tests to check if callback is ignored

--- test/Intlc/LinterSpec.hs
+++ test/Intlc/LinterSpec.hs
@@ -1,9 +1,20 @@
 module Intlc.LinterSpec where
-
+import           Intlc.Parser.ICU
 import           Intlc.ICU
 import           Intlc.Linter
 import           Prelude
 import           Test.Hspec
+import           Text.Megaparsec       (runParser)
+import           Intlc.Parser.Error    (ParseFailure)
+
+parseWith :: ParserState -> Parser a -> Text -> Either ParseFailure a
+parseWith s p = runParser (runReaderT p s) "test"
+
+parse :: Parser a -> Text -> Either ParseFailure a
+parse = parseWith initialState
+
+lintICUText :: Text -> Either ParseFailure Status
+lintICUText t = lint <$> (parse msg t)
 
 spec :: Spec
 spec = describe "linter" $ do
@@ -23,13 +34,13 @@ spec = describe "linter" $ do
     lint (Message [Interpolation "Hello" (Callback []), Plaintext "hello"]) `shouldBe` Success
 
   it "does not lint streams with 2 or more complex interpolations" $ do
-    lint (Message [Interpolation "Hello" (Callback []), Interpolation "Hello" (Bool [] [])]) `shouldBe` Failure (pure TooManyInterpolations)
+    lint (Message [Interpolation "Hello" (Bool [] []), Interpolation "Hello" (Bool [] [])]) `shouldBe` Failure (pure TooManyInterpolations)
 
   it "does not lint nested streams" $ do
-    lint (Message [Interpolation "outer" (Callback [Interpolation "inner" (Callback [])])]) `shouldBe` Failure (pure TooManyInterpolations)
+    lint (Message [Interpolation "outer" (Bool [Interpolation "inner" (Bool [] [])] [])]) `shouldBe` Failure (pure TooManyInterpolations)
 
   it "does not lint complex interpolations with nested complex interpolations" $ do
-    lint (Message [Interpolation "outer" (Select (fromList [SelectCase "hello" [Interpolation "super_inner" (Callback [])]]) Nothing)]) `shouldBe` Failure (pure TooManyInterpolations)
+    lint (Message [Interpolation "outer" (Select (fromList [SelectCase "hello" [Interpolation "super_inner" (Bool [] [])]]) Nothing)]) `shouldBe` Failure (pure TooManyInterpolations)
 
   it "lints plural" $ do
     lint (Message $
@@ -51,12 +62,11 @@ spec = describe "linter" $ do
     lint (Message [Plaintext "Message with an emoji ❤️ 🥺"]) `shouldBe` Failure (pure $ InvalidNonAsciiCharacter(fromList "❤️🥺"))
 
   it "does not lint text that is deeply nested with emoji" $ do
-    lint (Message [Interpolation "Hello" (Callback []), Interpolation "Hello" (Bool [Plaintext "Message with an emoji 🥺"] [])]) `shouldBe` Failure (fromList [TooManyInterpolations,InvalidNonAsciiCharacter( fromList ['🥺'])])
+    lint (Message [Interpolation "Hello" (Bool [] []), Interpolation "Hello" (Bool [Plaintext "Message with an emoji 🥺"] [])]) `shouldBe` Failure (fromList [TooManyInterpolations,InvalidNonAsciiCharacter( fromList ['🥺'])])
 
   it "lints streams without emoji" $ do
     lint (Message [Plaintext "Text without emoji"]) `shouldBe` Success
     
-
 
   it "stops iterating after encountering two stream-interpolations" $ do
     let nested x = Interpolation "x" (Callback [x])
@@ -68,3 +78,5 @@ spec = describe "linter" $ do
       ]) `shouldBe` Failure (pure TooManyInterpolations)
 
 
+  it "lints multiple and nested callback tags" $ do
+    lintICUText ("<strong>High <div></div>quality photos</strong> (at least <strong>{megapixels}MP</strong>)") `shouldBe` Right Success
