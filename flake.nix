46556e680af96b132a35337d1fd4fa794c438e18
Sam A. Horvath-Hunt <hello@samhh.com>
2022-07-27 13:17:10 +0100

    Downgrade to GHC 9.2.2 for easier HLS compatibility
    See comment in `flake.nix`. This makes HLS compatibility easier for
    everyone regardless of whether or not they're using Nix to obtain it.

--- flake.nix
+++ flake.nix
@@ -12,9 +12,15 @@
           buildInputs = with pkgs; [
             git
 
-            haskell.compiler.ghc923
-            haskell.packages.ghc923.cabal-install
-            haskell.packages.ghc923.hspec-golden
+            # HLS 1.7.0.0 via ghcup doesn't supply a 9.2.3 binary.
+            #
+            # HLS via Nix is currently unavailable on ARM/M1 on 9.2.3:
+            #   https://github.com/NixOS/nixpkgs/issues/140774#issuecomment-1186546139
+            #
+            # Thus we've pinned nixpkgs to the last commit before 9.2.2 was bumped up to 9.2.3.
+            haskell.compiler.ghc922
+            haskell.packages.ghc922.cabal-install
+            haskell.packages.ghc922.hspec-golden
 
             # For typechecking golden output
             nodejs
