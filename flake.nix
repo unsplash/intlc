bbffc6e54340219e5908431388c7e0376bc64c91
Sam A. Horvath-Hunt <hello@samhh.com>
2022-10-12 14:45:14 +0100

    Add HLS to dev shell
    Installing it per-project is preferred as HLS is bound quite tightly to specific versions of GHC.

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
 
             haskell.compiler.ghc8107
             haskell.packages.ghc8107.cabal-install
+            haskell.packages.ghc8107.haskell-language-server
             haskell.packages.ghc8107.hspec-golden
 
             # For typechecking golden output
