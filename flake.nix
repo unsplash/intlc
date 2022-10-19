c334c9b48b370c1834fbb58a436eb34aa096d1db
Sam A. Horvath-Hunt <hello@samhh.com>
2022-10-19 14:58:41 +0100

    Only include HLS for our GHC compiler version

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
+          ghcVer = "924";
           # HLS in nixpkgs is marked as broken on aarch64-darwin via LLVM 7,
           # see:
           #   https://github.com/NixOS/nixpkgs/blob/a410420844fe1ad6415cf9586308fe7538cc7584/pkgs/development/compilers/llvm/7/compiler-rt/default.nix#L108
@@ -14,7 +15,7 @@
           # See also in unsplash/intlc: #162, #167
           hls = if system == flake-utils.lib.system.aarch64-darwin
             then [ ]
-            else [ pkgs.haskell-language-server ];
+            else [ (pkgs.haskell-language-server.override { supportedGhcVersions = [ ghcVer ];}) ];
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
@@ -22,7 +23,7 @@
 
             # Ensure that everything here has a binary cached in Hydra on all
             # supported architectures; avoid `haskell.packages.ghcXXX.package`.
-            haskell.compiler.ghc924
+            haskell.compiler."ghc${ghcVer}"
             cabal-install
             haskellPackages.hspec-golden
 
