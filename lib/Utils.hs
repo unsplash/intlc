288415db75c28cd9f0ad38d633d4fd3474f2bbe9
Sam A. Horvath-Hunt <hello@samhh.com>
2022-10-25 21:28:25 +0100

    Add bun[By] utils

--- lib/Utils.hs
+++ lib/Utils.hs
@@ -1,5 +1,6 @@
 module Utils where
 
+import           Data.List (nubBy, (\\))
 import           Prelude
 
 -- Borrowed from: https://hackage.haskell.org/package/intro-0.9.0.0/docs/Intro.html#v:-60--62--94-
@@ -11,3 +12,13 @@ infixr 6 <>^
 
 apply2 :: a -> b -> (a -> b -> c) -> c
 apply2 x y f = f x y
+
+-- | Filters out the first instance of each found value as per the predicate.
+-- The dual of `nubBy`.
+bunBy :: (Foldable f, Eq a) => (a -> a -> Bool) -> f a -> [a]
+bunBy f xs = let ys = toList xs in ys \\ nubBy f ys
+
+-- | Filters out the first instance of each found value as per its `Eq`
+-- instance. The dual of `nub`.
+bun :: (Foldable f, Eq a) => f a -> [a]
+bun = bunBy (==)
